92d606ff6dae4cbd6b1a0033a13296bdfabfbaf5
Harun Radoncic <hradoncic@mjhlifesciences.com>
2025-10-07 17:07:58 -0500

    Add user modal w/ journal & check list section

--- skilltree-app/src/App.jsx
+++ skilltree-app/src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import "./App.css";
-import { UserProfile } from "./UserProfile";
+import React from 'react';
+import './App.css';
+import { UserProfile } from './UserProfile';
+import { SkillTreeButton } from './SkillTreeButton';
 
 function App() {
   return (
     <div className="App">
       <UserProfile />
+      <SkillTreeButton />
     </div>
   );
 }
