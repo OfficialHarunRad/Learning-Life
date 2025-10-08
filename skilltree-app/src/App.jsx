7c66dd37e3132c4c1edc0e10813604c195f492e9
Harun Radoncic <hradoncic@mjhlifesciences.com>
2025-10-08 18:28:49 -0500

    Add user info bar

--- skilltree-app/src/App.jsx
+++ skilltree-app/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { UserProfile } from './UserProfile';
 import { SkillTreeButton } from './SkillTreeButton';
+import { UserInfoBar } from './UserInfoBar';
 
 function App() {
   return (
     <div className="App">
       <UserProfile />
       <SkillTreeButton />
+      <UserInfoBar />
     </div>
   );
 }
